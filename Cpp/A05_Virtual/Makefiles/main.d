53bcc3a766ad66eccf989c2f12fad19bf87d33f0
flake.yeh <flake.yeh@syncomm.com.tw>
2016-04-06 20:53:47 +0800

    [8051][Make] Add example

--- Cpp/A05_Virtual/Makefiles/main.d
+++
@@ -1,3 +0,0 @@
-main.o: src/main.cpp include/My_Basics.hpp include/My_Basics.h \
- include/My_Macros.h include/My_MacroFuncs.h include/My_Types.h
-	g++ -Iinclude -c -g -Wall -std=c++11 src/main.cpp -o build/main.o 
