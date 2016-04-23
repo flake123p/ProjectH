1ff1533dcc9ef1674be37ac67e763bf6cc15f038
flake.yeh <flake.yeh@syncomm.com.tw>
2016-04-23 11:20:29 +0800

    [Make] Add A05, [Verilog] Add A09, A10

--- Verilog/A09_And_An_Array/test_tb.v
+++ Verilog/A09_And_An_Array/test_tb.v
@@ -2,10 +2,10 @@
 `timescale 1ns/10ps
 module simple_tb;
 
-	reg   In1, In2, Select;
+	reg   In1, In2, In3;
 	wire  Out;
 
-	mux_2to1_beh M1(.OUT(Out), .IN1(In1), .IN2(In2), .Select(Select)); // Instance name required (M1)
+	mux_2to1_beh M1(Out, In1, In2, In3); // Instance name required (M1)
 
 	initial
 		// Block statement (Execute order: left to right, top to down)
@@ -13,15 +13,15 @@ module simple_tb;
 			$dumpfile("simple.vcd");
 			$dumpvars(0, M1);
 
-			#100 {Select, In1, In2} = 3'b000;
+			#100 {In3, In1, In2} = 3'b000;
 			repeat(7)
 			begin
-				#50 {Select, In1, In2} = {Select, In1, In2} + 1;
+				#50 {In3, In1, In2} = {In3, In1, In2} + 1;
 			end
 		end
 
 	initial begin
-		$monitor("time = %0d, Select=%b, In1=%b, In2=%b, Out=%b", $time, Select, In1, In2, Out);
+		$monitor("time = %0d, In3=%b, In1=%b, In2=%b, Out=%b", $time, In3, In1, In2, Out);
 	end
 	// Total time
 	initial #500 $finish;
