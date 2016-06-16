ddbdaaec759cb0feb90bf4e042a7849bd40cafb9
ocean909 <neriver1984@gmail.com>
2016-06-17 01:07:43 +0800

    class usage

--- Cpp/B01_Stack/src/main.cpp
+++ Cpp/B01_Stack/src/main.cpp
@@ -7,66 +7,94 @@ using namespace std;
 
 #define N 6
 
-void create_empty_stack(void);
-void push(int key);
-int pop(void);
-
-struct node{
+class node{
+	friend class list;
 	int data;
-	struct node *link;
+	node *link;
 };
 
-node *top, *rear;
+class list{
+	private:
+		node *top;
+	public:
+		list();
+		~list();
+		bool empty();
+		void push(int key);
+		int pop();
+};
 
-int main(int argc, char *argv[])
+list::list(void)
 {
-	int i;
-	int arr[N] = {10, 11, 12, 13, 14, 15};
-	
-	create_empty_stack();
-	
-	cout << endl << "put data to stack ...  " << endl;
-	for(i=0; i<N; i++){
-		cout << "sequence <" << i << "> put " << i << endl;
-		push(arr[i]);
-	}
-	
-	cout << endl << "get data from top side...  " << endl;
-	i = 0;
-	
-	while(top->link){
-		cout << "sequence <" << i++ << "> get " << pop() << endl;
-	}
-	delete top;
-	
-	return 0;
-}
-
-void create_empty_stack(void)
-{
-	top = new (struct node);
+	top = new node;
 	top->link = NULL;
 }
 
-void push(int key)
+list::~list(void)
 {
-	struct node *new_node;
+	node *link_node, *temp_node;
 	
-	new_node = new (struct node);
-	new_node->data = data;
+	if(top->link != NULL)
+	{
+		link_node = top->link;
+		while(link_node->link != NULL)
+		{
+			temp_node = link_node;
+			link_node = link_node->link;
+			delete temp_node;
+		}
+		delete link_node;
+	}
+	delete top;
+}
+
+bool list::empty(void)
+{
+	if(top->link == NULL)
+		return true;
+	else
+		return false;
+}
+
+void list::push(int key)
+{
+	node *new_node;
+	new_node = new node;
+	new_node->data = key;
 	new_node->link = top->link;
 	top->link = new_node;
 }
 
-int pop(void)
+int list::pop(void)
 {
 	int key;
-	struct node *temp_node;
-	
+	node *temp_node;
 	temp_node = top->link;
 	top->link = temp_node->link;
 	key = temp_node->data;
 	delete temp_node;
+	return key;
+}
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int arr[N] = {11, 12, 13, 14, 15, 16};
+	list stack;
+	cout << "put data into stack..." << endl;
+	for(i=0; i<N; i++)
+	{
+		cout << "move <" << arr[i] << "> to stack " << i << endl;
+		stack.push(arr[i]);
+	}
 	
-	return(key);
+	cout << "get data from top side..." << endl;
+	i = 5;
+	
+	while(!stack.empty())
+	{
+		cout << "get <" << stack.pop() << "> from stack " << i-- << endl;
+	}
+	
+	return 0;
 }
