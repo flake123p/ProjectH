df6ed79cbac6e9dbad784bb451885648fbc71a5e
Flake_MCHP <flake123p@gmail.com>
2016-12-23 13:26:24 +0800

    [Python] Add A29 example & useful little script.

---
+++ Python/_Basics_/A29_CommandLine_Parameter/test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import sys
+
+#usage: test.py <inputfile> <outputfile>
+
+print('Number of arguments:', len(sys.argv), 'arguments.')
+print('Argument List:', str(sys.argv))
+print('1st Argument:', str(sys.argv[0]))
+print('2nd Argument:', str(sys.argv[1]))
+print('3rd Argument:', str(sys.argv[2]))
