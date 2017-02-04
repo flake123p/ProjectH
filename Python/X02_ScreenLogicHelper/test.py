736730dfa6eb256b4b0b762ca678a4bd6ed9704a
Flake_dorm <flake123p@gmail.com>
2017-02-04 16:44:50 +0800

    Update X02_ScreenLogicHelper

--- Python/X02_ScreenLogicHelper/test.py
+++ Python/X02_ScreenLogicHelper/test.py
@@ -1,53 +1,73 @@
-def CreateFile(fileName, funcName, index):
-	fout = open(fileName, 'w')
-	fout.write('\nint Scrn'+ '{:02d}'.format(index) + funcName + '_OnEntry(IN  u32 ScrnEvtId, IN  void *ScrnEvtHdl)\n{\n\treturn 0;\n}\n')
-	fout.write('\nint Scrn'+ '{:02d}'.format(index) + funcName + '_OnDraw(IN  u32 ScrnEvtId, IN  void *ScrnEvtHdl)\n{\n\treturn 0;\n}\n')
-	fout.write('\nint Scrn'+ '{:02d}'.format(index) + funcName + '_OnEvent(IN  u32 ScrnEvtId, IN  void *ScrnEvtHdl)\n{\n\treturn 0;\n}\n')
-	fout.write('\nint Scrn'+ '{:02d}'.format(index) + funcName + '_OnLeave(IN  u32 ScrnEvtId, IN  void *ScrnEvtHdl)\n{\n\treturn 0;\n}\n')
-			
-fin = open('in.txt')
-ctr = 0
-for x in fin:
-	y = x.split()
-	z = ""
-	z = z.join(y)
-	outFileName = 'Scrn' + '{:02d}'.format(ctr) + z + '.c'
-	CreateFile(outFileName, z, ctr)
-	ctr += 1
+name_list = []
+up_name_list = [] # Uppercase which been used in typedef enum
 
-ctr = 0
+fin = open('__src_screen.txt')
+for each_line in fin:
+	# Produce name list
+	each_word_list = each_line.split()
+	concat_string = ""
+	concat_string = concat_string.join(each_word_list) # Scrn 00 Aaa Xxx ==> Scrn00AaaXxx
+	name_list.append(concat_string)
+	# Produce uppercase list
+	upper_list = []
+	for each_word in each_word_list:
+		upper_list.append(each_word.upper())
+	up_concat_string = "_"
+	up_concat_string = up_concat_string.join(upper_list) # Scrn 00 Aaa Xxx ==> SCRN_00_AAA_XXX
+	up_name_list.append(up_concat_string)
 fin.close()
-fin = open('in.txt')
+
+#Create Cpp File
+for each_word in name_list:
+	fout = open(each_word + '.cpp', 'w')
+	fout.write('int ' + each_word + '_OnEntry(IN  u32 ScrnEvtId, IN  void *ScrnEvtHdl)\n{\n\treturn 0;\n}\n\n')
+	fout.write('int ' + each_word + '_OnDraw(IN  u32 ScrnEvtId, IN  void *ScrnEvtHdl)\n{\n\treturn 0;\n}\n\n')
+	fout.write('int ' + each_word + '_OnEvent(IN  u32 ScrnEvtId, IN  void *ScrnEvtHdl)\n{\n\treturn 0;\n}\n\n')
+	fout.write('int ' + each_word + '_OnLeave(IN  u32 ScrnEvtId, IN  void *ScrnEvtHdl)\n{\n\treturn 0;\n}\n\n')
+
+# Let PaPa see the result.
+print('name_list    = ', name_list)
+print('up_name_list = ', up_name_list)
+
+#
+# Write sets.txt  1. cpp filename list
+#
 fout_sets = open('sets.txt', 'w')
-fout_sets.write('typedef enum {\n')
-for x in fin:
-	y = x.split()
-	z = "_"
-	z = z.join(y) + ','
+for each_word in name_list:
+	fout_sets.write('{:s}.cpp\n'.format(each_word))
+#
+# Write sets.txt  2. num
+#
+fout_sets.write('\n\ntypedef enum {\n')
+ctr = 0
+for each_word in up_name_list:
 	if ctr == 0:
-		z = z.join(y) + ' = 0,'
-		fout_sets.write('\tSCRN_' + '{:02d}'.format(ctr) + '_' + '{:15s}'.format(z) + '// {:d}\n'.format(ctr))
+		string = '\t' + each_word + ' = 0,'
 	else:
-		z = z.join(y) + ','
-		fout_sets.write('\tSCRN_' + '{:02d}'.format(ctr) + '_' + '{:15s}'.format(z) + '// {:d}\n'.format(ctr))
+		string = '\t' + each_word + ','
+	fout_sets.write('{:30s}'.format(string) + '// {:d}\n'.format(ctr))
 	ctr += 1
 fout_sets.write('} SCRN_INDEX;\n\n')
-
-ctr = 0
-fin.close()
-fin = open('in.txt')
-fout_sets = open('sets.txt', 'a')
+#
+# Write sets.txt  3. callback array
+#
 fout_sets.write('SCREEN_CB_LIST_t My_ScreenCallbackList[] = {\n')
-for x in fin:
-	y = x.split()
-	z = "_"
-	z = z.join(y) + ','
-	funcName = ""
-	funcName = funcName.join(y)
-	fout_sets.write('\t{SCRN_' + '{:02d}'.format(ctr) + '_' + '{:s}, 0, '.format(z))
-	fout_sets.write('Scrn'+ '{:02d}'.format(ctr) + funcName + '_OnEntry, ')
-	fout_sets.write('Scrn'+ '{:02d}'.format(ctr) + funcName + '_OnDraw, ')
-	fout_sets.write('Scrn'+ '{:02d}'.format(ctr) + funcName + '_OnEvent, ')
-	fout_sets.write('Scrn'+ '{:02d}'.format(ctr) + funcName + '_OnLeave},\n')
+ctr = 0
+for each_word in name_list:
+	fout_sets.write('\t')
+	fout_sets.write('{' + '{:20} 0, '.format(up_name_list[ctr] + ','))
+	fout_sets.write('{:28s}'.format(each_word + '_OnEntry, '))
+	fout_sets.write('{:28s}'.format(each_word + '_OnDraw, '))
+	fout_sets.write('{:28s}'.format(each_word + '_OnEvent, '))
+	fout_sets.write('{:s}'.format(each_word + '_OnLeave},\n'))
 	ctr += 1
-fout_sets.write('};\n')
+fout_sets.write('};\n\n')
+#
+# Write sets.txt  4. callback function declaration list
+#
+for each_word in name_list:
+	fout_sets.write('int ' + each_word + '_OnEntry(IN  u32 ScrnEvtId, IN  void *ScrnEvtHdl);\n')
+	fout_sets.write('int ' + each_word + '_OnDraw(IN  u32 ScrnEvtId, IN  void *ScrnEvtHdl);\n')
+	fout_sets.write('int ' + each_word + '_OnEvent(IN  u32 ScrnEvtId, IN  void *ScrnEvtHdl);\n')
+	fout_sets.write('int ' + each_word + '_OnLeave(IN  u32 ScrnEvtId, IN  void *ScrnEvtHdl);\n')
+fout_sets.close()
--- Python/_Basics_/A09_File/test.py
+++ Python/_Basics_/A09_File/test.py
@@ -5,10 +5,22 @@ print('fin = ', fin)
 print('fin.readline() = ', fin.readline())
 print('fin.readline() = ', fin.readline())
 
-print('for x in fin:')
-for x in fin:
-	print('x.strip() = ', x.strip(), '  (Remove characters: \\r\\n)')
+# for each line in file
+print('for each_line in fin:')
+for each_line in fin:
+	print('each_line.strip() = ', each_line.strip(), '  (Remove characters: \\r\\n)')
+	#each word
+	each_word_list = each_line.split()
+	print('each_word_list = ', each_word_list)
+	#concat eachword
+	concat_string = '-'
+	concat_string = concat_string.join(each_word_list)
+	print('concat_string = ', concat_string)
 
 fout = open('write.txt', 'w')
 fout.write('XXX\n')
 fout.write('YYY')
+
+ctr = 13
+print('ctr = 0x{:08x}'.format(ctr))
+print('ctr = {:08d}'.format(ctr))
