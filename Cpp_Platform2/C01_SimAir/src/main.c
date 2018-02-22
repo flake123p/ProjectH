c81927f434b21c0b3cee73abd4be5364afb45683
Flake_MCHP <flake123p@gmail.com>
2018-02-22 22:25:55 +0800

    C01_SimAir 1st rev

---
+++ Cpp_Platform2/C01_SimAir/src/main.c
@@ -0,0 +1,15 @@
+
+#include "Everything_App.hpp"
+
+
+int main(int argc, char *argv[])
+{
+	//LibArgParser_OptionSet_Demo();
+	LibPacket_Demo();
+	//LibSysCmd_SimpleCmd("start /B entry.bat 1233");
+	//LibOs_SleepSeconds(2);
+	//LibSysCmd_SimpleCmd("start /B entry.bat 1235");
+	//LibSysCmd_Simple("python test.py");
+	return 0;
+}
+
