0157f73c7a757ee23232b3dee4342b2dd6c02d85
Flake_MCHP <flake123p@gmail.com>
2018-07-29 15:46:04 +0800

    CPP PF2x - update build script

--- Cpp_Platform2X/_app/A01_TestApp/clean.bat
+++ Cpp_Platform2X/_app/A01_TestApp/clean.bat
@@ -1,14 +1,5 @@
 @echo off
 
-rem Define paths
-SET LIB_PATH=..\..\..\Cpp_Platform2\_Library
-SET LOCAL_PATH=%CD%
-
-rem Do clear
-CD %LIB_PATH%
-CALL lib_clean.bat --DisablePause
-CD %LOCAL_PATH%
-
 echo ====== Clean Application ... ======
 del .\Makefiles\*.d              /F /Q
 del .\Makefiles\_00_Ext_Mods.mak /F /Q
