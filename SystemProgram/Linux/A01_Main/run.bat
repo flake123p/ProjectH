67faa81a92122583d51d227bf203538f4a26252c
John-Dell <flake123p@gmail.com>
2018-09-04 19:40:28 +0800

    Sys-Linux Add A01_Main

---
+++ SystemProgram/Linux/A01_Main/run.bat
@@ -0,0 +1,28 @@
+@ECHO OFF
+
+call build.bat %1 %2
+
+IF %result1% EQU 0 (
+IF %result2% EQU 0 (
+IF EXIST aout.exe (
+	goto EXECUTE_AOUT
+)
+)
+)
+:ELSE
+echo NOT execute aout.exe ...
+goto EXIT_NORMAL
+
+rem
+rem  Within a block statement (a parenthesised series of statements), the entire block is parsed and then executed.
+rem  So move aout.exe out to read ERRORLEVEL correctly
+rem
+:EXECUTE_AOUT
+aout.exe
+echo ====== aout.exe Done!! Errorlevel = %ERRORLEVEL% ======
+
+if "%2" NEQ "--DisablePause" (
+	pause
+)
+	
+:EXIT_NORMAL
---
+++ SystemProgram/Linux/A01_Main/src/main.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+#include <stdint.h>
+
+int main(int argc, char *argv[])
+{
+	printf("This is %s()\n", __func__);
+
+	return 0;
+}
